40853463c62c49113d2fafce35468b4ddffcb6f0
Byreds242 <ethan.seisdedos@estudiant.fjaverianas.com>
2025-05-15 20:45:01 +0200

    a2 commit

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -53,7 +53,6 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
